c085dc5c61957c072713858855444eecb02637e0
Jeff Yates <jeff.yates@alumni.manchester.ac.uk>
2016-03-24 03:01:58 -0500

    Removed some usings

--- src/PressRelease/Controllers/HomeController.cs
+++ src/PressRelease/Controllers/HomeController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
+﻿using System.Security.Claims;
 using Microsoft.AspNet.Mvc;
-using PressRelease.Models;
 
 namespace PressRelease.Controllers
 {
