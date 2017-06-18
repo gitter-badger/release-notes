7d08b0618d93bd93eeeb96351c26fb93940d6e7a
Jeff Yates <jeff.yates@alumni.manchester.ac.uk>
2017-06-17 20:21:04 -0500

    Quick test for grabbing access token

--- PressRelease/Controllers/HomeController.cs
+++ PressRelease/Controllers/HomeController.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Octokit;
+using PressRelease.Models;
+using AspNet.Security.OAuth.GitHub;
 
 namespace PressRelease.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("")]
-        public IActionResult Index()
+        private readonly IGitHubClient _github;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public HomeController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IGitHubClient github)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _github = github;
+        }
+
+        [Route("")]
+        public async Task<IActionResult> Index()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var token = await _userManager.GetAuthenticationTokenAsync(
+                    user,
+                 GitHubAuthenticationDefaults.AuthenticationScheme,
+                  "access_token");
+            }
             return View();
         }
 
